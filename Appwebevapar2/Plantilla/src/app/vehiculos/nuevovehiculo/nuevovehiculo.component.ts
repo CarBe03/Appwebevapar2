a9e0d5c559df0256ee6b1ebc95a5a988c71e1428
CarBe03 <carolopintag2003@hotmail.com>
2024-09-18 19:16:51 -0500

    Evaluacion

---
+++ Appwebevapar2/Plantilla/src/app/vehiculos/nuevovehiculo/nuevovehiculo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevovehiculo',
+  standalone: true,
+  imports: [],
+  templateUrl: './nuevovehiculo.component.html',
+  styleUrl: './nuevovehiculo.component.scss'
+})
+export class NuevovehiculoComponent {
+
+}
